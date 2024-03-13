420c7a9f0ec6b235fc47ebf98e4a2099833282a0
Медведев Иван <ranzar2000@gmail.com>
2024-03-14 01:37:51 +0300

    Create an overused List component
    Generics, generalized file types

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
 import Card, { CardVariant } from "./components/Card";
-import UserList from "./components/UserList";
-import { IUser } from "./types/types";
+import { ITodo, IUser } from "./types/types";
 import axios from "axios";
+import List from "./components/List";
+import User from "./components/User";
+import TodoItem from "./components/TodoItem";
 
 function App() {
   const [users, setUsers] = useState<IUser[]>([]);
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
   useEffect(() => {
     fetchUsers();
+    fetchTodos();
   }, []);
 
   async function fetchUsers() {
@@ -16,9 +20,16 @@ function App() {
       const response = await axios.get<IUser[]>("https://jsonplaceholder.typicode.com/users");
       setUsers(response.data);
     } catch (error) {
-      
-    } finally {
+      console.log((error as Error).message);
+    }
+  }
 
+  async function fetchTodos() {
+    try {
+      const response = await axios.get<ITodo[]>("https://jsonplaceholder.typicode.com/todos?_limit=10");
+      setTodos(response.data);
+    } catch (error) {
+      console.log((error as Error).message);
     }
   }
 
@@ -30,11 +41,17 @@ function App() {
         height="200px"
         onClick={(num) => { console.log('num ' + num) }}
       >
-        <button>Кнопка</button>
+        <button>К��опка</button>
       </Card>
 
-      <UserList 
-        users={users}
+      <List
+        items={users}
+        renderItem={(user: IUser) => <User user={user} key={user.id} />}
+      />
+
+      <List
+        items={todos}
+        renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id} />}
       />
     </div>
   )
